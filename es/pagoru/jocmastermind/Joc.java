1c44cafad90831f5629bfe835cbe6460fc7f22e3
pagoru <pagoru@gmail.com>
2016-06-01 20:08:43 +0200

    ++
    Signed-off-by: pagoru <pagoru@gmail.com>

--- es/pagoru/jocmastermind/Joc.java
+++ es/pagoru/jocmastermind/Joc.java
@@ -1,6 +1,5 @@
 package es.pagoru.jocmastermind;
 
-import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -35,56 +34,84 @@ public class Joc {
 	public void run(){
 		
 		System.out.println("Jugador 1:");
-		chooseColors();
+		chooseColorsFirst();
 		System.out.println("Jugador 2:");
 		
-		for (int i = MAX_ATTEMPTS; i > 0; i--) {
-			String out = "[" + i + "] ";
-			if(i < 10){
-				out += " ";
+		for (int o = MAX_ATTEMPTS; o > 0; o--) {
+			chooseColorsSecond();
+			
+			for (int i = MAX_ATTEMPTS; i > 0; i--) {
+				String out = "[" + i + "] ";
+				if(i < 10){
+					out += " ";
+				}
+				System.out.print(out);
+				for (int j = 0; j < MAX_COLORS; j++) {
+					System.out.print(getFirstUpper(colors[j]) + " ");
+				}
+				System.out.print("- ");
+				for (int j = 0; j < MAX_COLORS; j++) {
+					String pos = responseFromColorAttempts()[i - 1][j];
+					if(pos != null){
+						System.out.print(pos);
+					} else {
+						System.out.print("N ");
+					}
+				}
+				System.out.println();
 			}
-			System.out.print(out);
-			for (int j = 0; j < MAX_COLORS; j++) {
-				System.out.print(getFirstUpper(colors[j]) + " ");
-			}
-			System.out.print("- ");
-			for (int j = 0; j < MAX_COLORS; j++) {
-				System.out.print("X ");
-			}
-			System.out.println();
 		}
 		
 	}
-	/*
-	private String[] getResponseFromAttempt(int attempt){
-		int[] countColors = getRecountColors(colors);
+	private String[][] responseFromColorAttempts(){
+		String[][] 	res 	= new String[MAX_ATTEMPTS][MAX_COLORS];
+		int[] 		count 	= countColorsFromCurrent();
+		//1* Mateixa posició 'V'
 		for (int i = 0; i < colors.length; i++) {
-			
-			for (int j = 0; j < COLORS_DEFAULT.length; j++) {
-				if(countColors[j] > 0){
-					if(COLORS_DEFAULT[j].equalsIgnoreCase(colors[i])){
-						countColors[j]--;
+			String col = attemptsColors[currentAttempt][i];
+			int indexCol = getColorIndex(col);
+			if(indexCol != -1){
+				if(colors[i].equalsIgnoreCase(col)){
+					res[currentAttempt][i] = "V ";
+					count[indexCol]--;
+				} else {
+					if(count[indexCol] > 0){
+						//2* No esta en la posició '+'
+						res[currentAttempt][i] = "+ ";
+					} else {
+						//3* No es correcte 'X'
+						res[currentAttempt][i] = "X ";
 					}
 				}
+			}	
+		}
+		return res;
+	}
+	private int getColorIndex(String color){
+		for (int i = 0; i < COLORS_DEFAULT.length; i++) {
+			if(COLORS_DEFAULT[i].equalsIgnoreCase(color)){
+				return i;
+			}
+		}
+		return -1;
+	}
+	private int[] countColorsFromCurrent(){
+		int[] count = new int[COLORS_DEFAULT.length];
+		for (int i = 0; i < COLORS_DEFAULT.length; i++) {
+			
+			for (int j = 0; j < colors.length; j++) {
+				if(COLORS_DEFAULT[i].equalsIgnoreCase(colors[j])){
+					count[i]++;
+				}
 			}
 			
 		}
-	}*/
-	private int[] getRecountColors(String[] colors){
-		int[] countColors = new int[COLORS_DEFAULT.length];
-		for (int i = 0; i < colors.length; i++) {
-			for (int j = 0; j < COLORS_DEFAULT.length; j++) {
-				if(COLORS_DEFAULT[j].equalsIgnoreCase(attemptsColors[currentAttempt][i])){
-					countColors[j]++;
-				}
-			}
-		}
-		return countColors;
+		return count;
 	}
 	private String getFirstUpper(String color){
 		return color.substring(0, 1).toUpperCase();
 	}
-	private void chooseColors(){
+	private void chooseColorsFirst(){
 		int maxColors = 0;
 		do {
 			System.out.println("Introdueix el color " + (maxColors + 1) + ":");
@@ -96,6 +123,21 @@ public class Joc {
 				System.out.println("El color no es valid!");
 			}
 		} while (maxColors < MAX_COLORS);
+		
+	}
+	private void chooseColorsSecond(){
+		int maxColors = 0;
+		do {
+			System.out.println("Introdueix el color " + (maxColors + 1) + ":");
+			String currentColor = readString();
+			if(isColorValid(currentColor)){
+				attemptsColors[currentAttempt][maxColors] = currentColor;
+				maxColors++;
+			} else {
+				System.out.println("El color no es valid!");
+			}
+		} while (maxColors < MAX_COLORS);
+		currentAttempt++;
 		
 	}
 	public boolean isColorValid(String color){
